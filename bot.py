ee0b1af9c86c97b00a9d7cf4c39c7ae3e65334c5
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2022-08-04 20:39:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,10 +39,10 @@ CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).sp
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
-@pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
+@pr0fess0r_99.on_message(filters.private & filters.command(["kundan"]))
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
-    button = [[ InlineKeyboardButton("Devoloper 🖥️", url="https://t.me/dragadm"), InlineKeyboardButton("Updates ", url="t.me/rosebakthan") ],
+    button = [[ InlineKeyboardButton("paid service", url="https://t.me/dragadm"), InlineKeyboardButton("Updates ", url="t.me/rosebakthan") ],
               [ InlineKeyboardButton(" Add Me To Your Chat ", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://t.me/dragadm||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
